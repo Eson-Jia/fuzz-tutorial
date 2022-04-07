645815a01d6b45f417937c6ca885fdb2a8f9118a
jiayuxin <13520723393@163.com>
2022-04-07 19:30:01 +0800

    complete tutorial

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
-func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return "", errors.New("invalid UTF-8 string")
 	}
-	return string(b)
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
 }
 
 func main() {
 	input := "The quick brown fox jumps over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
 	fmt.Printf("original: %s\n", input)
-	fmt.Printf("reversed: %s\n", rev)
-	fmt.Printf("reversed again: %s\n", doubleRev)
+	fmt.Printf("reversed: %q,err: %v\n", rev, revErr)
+	fmt.Printf("double reversed: %q,err: %v\n", doubleRev, doubleRevErr)
 }
--- reverse_test.go
+++ reverse_test.go
@@ -5,32 +5,20 @@ import (
 	"unicode/utf8"
 )
 
-func TestReverse(t *testing.T) {
-	testcases := []struct {
-		in, want string
-	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{" ", " "},
-		//!123456789
-		{"!123456789", "987654321!"},
-	}
-	for _, tc := range testcases {
-		rev := Reverse(tc.in)
-		if rev != tc.want {
-			t.Errorf("Reverse: %q,want %q", rev, tc.want)
-		}
-	}
-}
-
 func FuzzReverse(f *testing.F) {
 	testcases := []string{"Hello, world", " ", "!12345"}
 	for _, tc := range testcases {
 		f.Add(tc)
 	}
 	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
-		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
 		if orig != doubleRev {
 			t.Errorf("Before: %q, After: %q", orig, doubleRev)
 		}
